c6c97df646d3e628b2e2027d64e77cc494611a9a
alexiopride <alexiopride@gmail.com>
2022-09-04 21:59:56 +0400

    Дополнил дз 3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Number is even"); 
+}
+else 
+{
+    Console.WriteLine("Number isn't even");
+}
