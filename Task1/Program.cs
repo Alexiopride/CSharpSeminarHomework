17a1153fdc9781c4edf58b7cbdcb459257298569
alexiopride <alexiopride@gmail.com>
2022-09-08 18:34:40 +0400

    Added task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -9,13 +9,13 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-int max = 0; 
-int min = 0;
+// int max = 0; 
+// int min = 0;
 
 if (num1 > num2) 
 {
-    max = num1;
-    min = num2;
+    // max = num1;
+    // min = num2;
     Console.Write("max = ");
     Console.WriteLine(num1);
     Console.Write("min = ");
@@ -23,8 +23,8 @@ if (num1 > num2)
 }
 else 
 {
-    max = num2;
-    min = num1;
+    // max = num2;
+    // min = num1;
     Console.Write("max = ");
     Console.WriteLine(num2);
     Console.Write("min = ");
